e13ecc97a547d3f37910681fd63f481a6196332a
Akash Gautam <gautamakash91@gmail.com>
2020-08-03 13:41:37 +0530

    redux structure

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,4 @@
 var UNIVERSAL = {
-	// BASEURL: "http://localhost:8008/"
-	// BASEURL: "https://socus.herokuapp.com/"
-	BASEURL: "http://192.168.1.5:8008/"
-	// BASEURL: "http://192.168.0.165:8008/"
-	//BASEURL: "http://192.168.0.136:8008/"
-	// BASEURL: "http://192.168.0.191:8008/"
+	BASEURL: "http://localhost:8008/"
 };
 export default UNIVERSAL;
